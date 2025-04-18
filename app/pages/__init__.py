4d2ba75ab27a1f8e401b2f1000100ed404d11ed4
Fábio Linhares <fl@ic.ufal.br>
2025-04-18 01:24:41 -0300

    Adiciona o arquivo __init__.py ao pacote app.pages.algorithms, permitindo a exportação explícita dos módulos de algoritmos.

--- app/pages/__init__.py
+++ app/pages/__init__.py
@@ -1,2 +1,9 @@
 # Arquivo __init__.py para o pacote app.pages
 # Este arquivo permite que o Python reconheça este diretório como um pacote importável
+
+# Exportar módulos explicitamente
+from . import main_app
+from . import about
+from . import haversine_page
+from . import report_page
+from .algorithms import astar_page, bfs_page, fuzzy_page
---
+++ app/pages/algorithms/__init__.py
@@ -0,0 +1,9 @@
+# Arquivo __init__.py para o pacote app.pages.algorithms
+# Este arquivo permite que o Python reconheça este diretório como um pacote importável
+
+# Exportar módulos explicitamente
+from . import astar_page
+from . import bfs_page
+from . import fuzzy_page
+from . import dijkstra_page
+from . import dfs_page
